e9fa193a1d1ec224f80ccf8a1179f91222f7bc94
Terence <piece4ulmind@gmail.com>
2023-08-21 11:26:28 -0400

    Added NBA Logo

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -7,6 +7,7 @@ function Header() {
         NBA Teams
         <span className="logo" role="img">
         </span>
+        <img src = "./images/NBA-Logo-1.jpg" alt="NBA" width="200" height="62"></img>
       </h1>
     </header>
   );
